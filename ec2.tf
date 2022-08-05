afe8add8bdab176964ba15abf207e672e0257a80
Partha Pratim Datta <92267112+devops-parth@users.noreply.github.com>
2022-08-05 14:24:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,14 +7,17 @@ resource "aws_instance" "myec2" {
   user_data = <<-EOF
     #!/bin/bash
     sudo apt update -y
+    sudo apt install docker.io -y
     sudo apt install apt-transport-https -y
     sudo apt upgrade -y
     sudo apt install virtualbox virtualbox-ext-pack -y
-    wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-    chmod +x minikube-linux-amd64
-    sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+    sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo chmod +x /home/ubuntu/minikube-linux-amd64
+    sudo cp minikube-linux-amd64 /usr/local/bin/minikube
     curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-    chmod +x ./kubectl
+    sudo chmod +x /home/ubuntu/kubectl
+    sudo cp kubectl /usr/local/bin/kubectl
+    sudo usermod -aG docker ubuntu
     minikube start
     sudo apt install git -y
     git init
